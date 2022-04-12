a699ff13f918649e575d67d264c7238f6126eb10
mickiy123 <mikikinfe345@gmail.com>
2022-04-12 21:56:21 +0300

    Add: complete task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Python script that, using the REST API
+    https://jsonplaceholder.typicode.com/, for a given employee ID,
+    returns information about his/her TODO list progress.
+"""
+
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+
+    response = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}"
+            .format(userId))
+
+    EmployeeId = response.json().get("id")
+    EmployeeName = response.json().get("name")
+    UserName = response.json().get("username")
+    FileName = userId + "." + "json"
+    print(FileName)
+
+    response1 = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    FinalDict = {}
+    AllList = []
+    for todo in response1.json():
+        if int(userId) == todo.get("userId"):
+            SingleDict = {}
+            SingleDict.update({"task": todo.get("title")})
+            SingleDict.update({"completed": todo.get("completed")})
+            SingleDict.update({"username": UserName})
+            AllList.append(SingleDict)
+    FinalDict.update({EmployeeId: AllList})
+
+    with open(FileName, "w") as jsonfile:
+        json.dump(FinalDict, jsonfile)
