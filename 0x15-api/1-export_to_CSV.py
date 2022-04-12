bcfb48d73bda6da16a90ffaf07f7fc8ac6aa5090
mickiy123 <mikikinfe345@gmail.com>
2022-04-12 20:30:24 +0300

    Add: complete task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Python script that, using the REST API
+    https://jsonplaceholder.typicode.com/, for a given employee ID,
+    returns information about his/her TODO list progress.
+"""
+
+if __name__ == '__main__':
+    import csv
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+
+    response = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}"
+            .format(userId))
+
+    EmployeeName = response.json().get("name")
+    UserName = response.json().get("username")
+    FileName = userId + "." + "csv"
+
+    response1 = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    allTasks = 0
+    completeTasks = 0
+    completeTasksList = []
+    rows = []
+    for todo in response1.json():
+        if int(userId) == todo.get("userId"):
+            allTasks += 1
+            NewList = []
+            NewList.append(str(todo.get("userId")))
+            NewList.append(UserName)
+            NewList.append(str(todo.get("completed")))
+            NewList.append(todo.get("title"))
+            rows.append(NewList)
+            if todo.get("completed"):
+                completeTasks += 1
+                completeTasksList.append(todo.get("title"))
+
+    with open(FileName, "w") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(rows)
