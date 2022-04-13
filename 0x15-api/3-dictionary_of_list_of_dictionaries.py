2e13c65ac9b6c8fee0e5657004f153dddd11d16b
mickiy123 <mikikinfe345@gmail.com>
2022-04-13 14:00:42 +0300

    Add: complete task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Python script that, using the REST API
+    https://jsonplaceholder.typicode.com/, for all employees,
+    returns information and save it to json file.
+"""
+
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+
+    response = requests.get(
+            "https://jsonplaceholder.typicode.com/users")
+
+    response1 = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    users = response.json()
+    todos = response1.json()
+    filename = "todo_all_employees.json"
+
+    AllTask = {}
+
+    for user in users:
+        TaskList = []
+        for task in todos:
+            if task.get("userId") == user.get('id'):
+                TaskDict = {"username": user.get("username"),
+                            "task": task.get("title"),
+                            "completed": task.get("completed")}
+            TaskList.append(TaskDict)
+        AllTask[user.get("id")] = TaskList
+
+    with open(filename, "w") as jsonfile:
+        json.dump(AllTask, jsonfile)
