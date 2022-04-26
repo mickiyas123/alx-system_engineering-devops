75efefb34ccf7788746f9faedfc20aae127fbd85
mickiy123 <mikikinfe345@gmail.com>
2022-04-26 17:50:34 +0300

    Fix: use if instead of try except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,13 @@ def number_of_subscribers(subreddit):
     import requests
     import json
 
-    try:
-        base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        response = requests.get(
-                base_url,
-                headers={'User-Agent': '0-subs:v0.0.0 (by /u/xyz_abc4890)'})
+    base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(
+            base_url,
+            headers={'User-Agent': '0-subs:v0.0.0 (by /u/xyz_abc4890)'})
+    if response.status_code != 200:
+        return 0
+    else:
         response = response.json()
         subscribers = response.get("data").get("subscribers")
         return subscribers
-    except ConnectionError:
-        return 0
