28457cedcd088c9f6fc10abb339ac24ac012c3f2
mickiy123 <mikikinfe345@gmail.com>
2022-04-29 15:21:01 +0300

    Add: add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,28 +8,29 @@ def recurse(subreddit, hot_list=[]):
        If an invalid subreddit is given, the function returns 0
     """
     import requests
-   
+
     count = 0
     root_url = "https://www.reddit.com/r"
     if len(hot_list) == 0 and count == 0:
         base_url = "{}/{}/hot/.json?limit=100".format(root_url, subreddit)
     else:
-        base_url = "{}/{}/hot/.json?limit=100&after={}".format(root_url, subreddit, hot_list[-1])
+        base_url = "{}/{}/hot/.json?limit=100&after={}".format(
+                root_url, subreddit, hot_list[-1])
     response = requests.get(
             base_url,
             headers={'User-Agent': '0-subs:v0.0.0 (by /u/xyz_abc4890)'},
             allow_redirects=False)
     if response.status_code != 200:
-        return "{}".format("None")
+        return "{}".format("OK")
     else:
         lst = []
         title_list = []
         response = response.json()
         for item in response.get('data').get('children'):
-            lst.append(item['kind'] + '_'  + item['data']['id'])
+            lst.append(item['kind'] + '_' + item['data']['id'])
         for post in response.get("data").get("children"):
             title_list.append(post.get("data").get("title"))
         return title_list
     count = count + 1
-    
+
     recurse(subreeddit, lst)
